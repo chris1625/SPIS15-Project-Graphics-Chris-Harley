8434bd9b687c54fe41b836432e4381df922e585d
Harley Saxton <fsaxton@ucsd.edu>
2015-08-24 14:38:22 -0700

    It works

--- newestSeamCarving.py
+++ newestSeamCarving.py
@@ -14,12 +14,15 @@ def greyscale(image):
             image.putpixel((x, y), (newRed, newGreen, newBlue))
     image.save('greyscale.jpg')
 
-def energyLevel(image):
+def energyLevel(image,widthchange):
     image=Image.open(image)
     (width,height)=image.size
-
+    endLocation = 0
+    endY = height - 1
+    #Important to be used later to determine coordinates of final position 
     '''Makes a table of the energy levels'''
-    table = [[0 for y in range(height)] for x in range(width)] 
+    table = [[0 for y in range(height)] for x in range(width)]
+    pathwaytable = [[0 for y in range(height)] for x in range(width)]
     for x in range (width-1):
         for y in range (height-1):
             energy = 0
@@ -40,17 +43,43 @@ def energyLevel(image):
 
     for a in range (height- 1):
         for b in range (width - 1):
+            john = table [b][a+1]
             if a != height - 1:
                 if b != 0 and b!= width - 2:
                     table[b][a+1] = table[b][a+1] + min(table[b-1][a],table[b][a],table[b+1][a])
+                    
+                    if table[b][a+1] - john == table[b-1][a]:
+                         pathwaytable[b][a+1] = [b - 1, a]
+                    elif table[b][a+1] - john == table[b+1][a]:
+                         pathwaytable[b][a+1] = [b + 1, a]
+                    else:
+                        pathwaytable[b][a+1] = [b , a]
+                        
                 elif b == 0:
-                    table[b][+1] = table[b][a+1] + min(table[b][a],table[b+1][a])
+                    table[b][a+1] = table[b][a+1] + min(table[b][a],table[b+1][a])
+                    if table[b][a+1] - john == table[b][a]:
+                         pathwaytable[b][a+1] = [b, a]
+                    else: 
+                         pathwaytable[b][a+1] = [b + 1, a]
                 else:
                     table[b][a+1] = table[b][a+1] + min(table[b-1][a],table[b][a])
-        
-
+                    if table[b][a+1] - john == table[b-1][a]:
+                         pathwaytable[b][a+1] = [b - 1, a]
+                    else:
+                         pathwaytable[b][a+1] = [b, a]
+                
     for c in range (width - 2):
         minimum = min(table[c][height - 1],table[c+1][height - 1])
-        
+        if min(table[c][height-1],table[c+1][height - 1]) == minimum:
+             endLocation = c
+
+    while endY != 0: 
+        image.putpixel((endLocation,endY),(255,0,0))
+        print pathwaytable[endLocation][endY]
+        print endLocation
+        print endY 
+        endLocation,endY = (pathwaytable[endLocation][endY][0],pathwaytable[endLocation][endY][1]) 
+    
+    image.show()
     return minimum 
     print minimum 
