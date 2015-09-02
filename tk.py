7fbcda85dd1ac13eddafc75b687f099ef0cfa5e9
Chris Harris <chris.h.4965@gmail.com>
2015-09-01 18:58:41 -0700

    Added some clarity, getting ready for integration

--- tk.py
+++ tk.py
@@ -18,11 +18,11 @@ def getFileName():
 def seamCarve(*args):
     width=int(inputWidth.get())
     height=int(inputHeight.get())
-    image=Image.open(fileOnly)
     print height,width
-    image.show()
     choice=str(option.get())
     print choice
+    # energyLevel(fileOnly,width)
+    # To be integrated later
 
 root=Tk()
 root.title("Harley and Chris' Seam Carving")
@@ -32,27 +32,32 @@ content.grid(column=0, row=0, sticky=(N,W,E,S))
 content.columnconfigure(0,weight=1)
 content.rowconfigure(0,weight=1)
 
-textlbl=ttk.Label(content,text="Please select your image:").grid(column=1,row=1, sticky=(W,E))
+textlbl6=ttk.Label(content,text="This program will take a width and/or height and shrink",font=('Arial','11','bold','italic'))
+textlbl6.grid(column=1,columnspan=2,row=1,sticky=(W))
+textlbl7=ttk.Label(content,text="or expand the image by those respective dimensions",font=('Arial','11','bold','italic'))
+textlbl7.grid(column=1,columnspan=2,row=2,sticky=(W))
+
+textlbl=ttk.Label(content,text="Please select your image:").grid(column=1,row=3, sticky=(W,E))
 address=StringVar()
-ttk.Button(content, text="Browse...",command=getFileName).grid(column=2, row=2, sticky=(W,E))
+ttk.Button(content, text="Browse...",command=getFileName).grid(column=2, row=4, sticky=(W,E))
 location=ttk.Label(content,text="No file selected",foreground='blue')
-location.grid(column=1,row=2,sticky=(W,E))
-textlbl2=ttk.Label(content,text="Please select an option and a width/height:").grid(column=1,row=3,sticky=(W,E))
+location.grid(column=1,row=4,sticky=(W,E))
+textlbl2=ttk.Label(content,text="Please select an option and a width/height:").grid(column=1,row=5,sticky=(W,E))
 
 option=StringVar()
 inputWidth=StringVar()
 inputHeight=StringVar()
 
-shrink=ttk.Radiobutton(content,text='Shrink',variable=option,value='shrink').grid(column=1,row=4,sticky=(W))
-expand=ttk.Radiobutton(content,text='Expand',variable=option,value='expand').grid(column=1,row=5,sticky=(W))
-textlbl4=ttk.Label(content,text="Height:").grid(column=1,row=4,sticky=(E))
+shrink=ttk.Radiobutton(content,text='Shrink',variable=option,value='shrink').grid(column=1,row=6,sticky=(W))
+expand=ttk.Radiobutton(content,text='Expand',variable=option,value='expand').grid(column=1,row=7,sticky=(W))
+textlbl4=ttk.Label(content,text="Height:").grid(column=1,row=6,sticky=(E))
 height_entry=ttk.Entry(content,width=6,textvariable=inputHeight)
-height_entry.grid(column=2,row=4,sticky=(W))
-textlbl3=ttk.Label(content,text="Width:").grid(column=1,row=5,sticky=(E))
+height_entry.grid(column=2,row=6,sticky=(W))
+textlbl3=ttk.Label(content,text="Width:").grid(column=1,row=7,sticky=(E))
 width_entry=ttk.Entry(content,width=6,textvariable=inputWidth)
-width_entry.grid(column=2,row=5,sticky=(W))
-textlbl5=ttk.Label(content,text="This may take a while...").grid(column=1,row=7,sticky=(E))
-ttk.Button(content,text="Seam Carve It!",command=seamCarve).grid(column=2,row=7,sticky=(E))
+width_entry.grid(column=2,row=7,sticky=(W))
+textlbl5=ttk.Label(content,text="This may take a while...").grid(column=1,row=8,sticky=(E))
+ttk.Button(content,text="Seam Carve It!",command=seamCarve).grid(column=2,row=8,sticky=(E))
 
 
 for child in content.winfo_children(): child.grid_configure(padx=4, pady=4)
